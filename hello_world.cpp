6abf231a1cf0c7fdb630b7abb71149a930bbf656
hexvgon666 <chelovekzhivotnoe@yandex.ru>
2023-10-14 02:30:18 +0300

    Change code style cpp files

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-int main() {
-	string name;
-	count << "Enter your name: ";
-	cin >> name;
-	count << "Hello world from@" << name << endl;
-	return 0;
+int
+main()
+{
+  string name;
+  count << "Enter your name: ";
+  cin >> name;
+  count << "Hello world from@" << name << endl;
+  return 0;
 }
